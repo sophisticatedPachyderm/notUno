4e31d38a430f6acb4313d32444b703ee3d2f6e17
Evan Moncuso <evan.moncuso@gmail.com>
2016-07-14 11:46:32 -0700

    Make fixes to user querys including adding single quotes and fixing async issues

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,4 +1,6 @@
 var router = require('express').Router();
+var db = require('./../db/userModel.js');
+// delete
 
 router.get('/chat', function(req, res) {
   console.log('GET request for /api/user/chat');
@@ -12,13 +14,13 @@ router.post('/chat', function(req, res) {
 
 router.get('/auth', function(req, res) {
   console.log('GET request for /api/user/auth');
-  res.end();
+  db.verifyPassword('docBrown', 'crazy', res);
 });
 
 router.post('/auth', function(req, res) {
   console.log('POST request for /api/user/auth');
-  res.end();
+  // this will create a new user in the database
+  db.newUser(req.body.username, req.body.password, res);
 });
-
 
 module.exports = router;
